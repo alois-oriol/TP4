1051ffe32c71e8df5a2163b663f4a6596a934e5e
alois-oriol <alois.oriol@gmail.com>
2025-11-14 10:33:13 +0100

    include

--- index.php
+++ index.php
@@ -1,24 +1,4 @@
-<!DOCTYPE html>
-<html lang="fr" prefix="og: https://ogp.me/ns#">
-<head>
-  <link rel="stylesheet" href="style.css">
-  <meta charset="utf-8" />
-  <meta name="author" content="Mathieu MANGEOT" />
-  <meta name="keywords" content="document html5 mmi" />
-  <meta name="description" content="Document minimal html5 en DUT MMI" />
-  <meta name="viewport" content="width=device-width, initial-scale=1">
 
-  <meta property="og:type" content="website" />
-  <meta property="og:url" content="http://mmi.univ-smb.fr/~mmang/" />
-  <meta property="og:image" content="http://mmi.univ-smb.fr/~mmang/image.png" />
-  <meta property="og:locale" content="fr_FR" />
-  <meta property="og:description" content="Document minimal html5 en DUT MMI" />
-  <meta property="og:title" content="Document minimal en MMI" />
-  <meta property="og:site_name" content="Document minimal en MMI" />
-  <title>Document minimal en MMI</title>
-</head>
-
-<body>
     <header>
         <h1>TP Numéro 4</h1>
         <nav>
@@ -40,8 +20,4 @@
         </form>
     </section>
         
-    <footer>
-        <p>Copyright © Aloiïs ORIOL</p>
-    </footer>
-</body>
-</html>
+   
---
+++ piedpage.php
@@ -0,0 +1,5 @@
+ <footer>
+        <p>Copyright © Aloïs ORIOL</p>
+    </footer>
+</body>
+</html>
